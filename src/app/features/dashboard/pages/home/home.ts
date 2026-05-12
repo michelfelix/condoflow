b3d3c5630978f9e9e31b9c195c836cdace33de57
Michel <michel.dias@envisiontecnologia.com.br>
2026-05-12 20:44:53 -0300

    Criado um input de busca para entender o formcontrol para um unico input e o método debouncetime do rxjs

--- src/app/features/dashboard/pages/home/home.ts
+++ src/app/features/dashboard/pages/home/home.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  searchControl = new FormControl('');
+
+  ngOnInit() {
+    this.searchControl.valueChanges
+    .pipe(
+
+      debounceTime(500),
+
+      distinctUntilChanged()
+
+    )
+    .subscribe(value => {
+
+      console.log('Buscar:', value);
+
+    });
+  }
+}
