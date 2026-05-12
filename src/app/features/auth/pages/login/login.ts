d7751d98e5521255fd100ce8e48605a59834def7
Michel <michel.dias@envisiontecnologia.com.br>
2026-05-11 21:15:31 -0300

    Importado o reativeforms do angular e seus componentes como formbuilder, formgroup e validators para a criação de um formulário totalmente reativo

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -1,29 +1,48 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../../../core/services/auth';
 import { Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class LoginComponent {
 
   constructor(
+    private fb: FormBuilder,
     private authService: AuthService,
     private router: Router
   ) {}
-
+  loginForm!: FormGroup;
+  
   onLogin(): void {
+    
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    console.log(this.loginForm.value);
+
     this.authService.login();
 
     this.router.navigate(['/dashboard']);
   }
 
   ngOnInit(): void {
+
     this.authService.isLoggedIn$.subscribe(value => {
       console.log('Usuário logado?', value);
     });
+
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
+  
 }
